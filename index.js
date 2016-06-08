16dd86abd3f0b7bf7d4d5f77694c90adf1921414
Tim Finley <tfinley@hubspot.com>
2016-06-08 00:01:17 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,275 @@
+const path = require('path');
+const nodeFS = require('fs');
+
+const unique  = require('lodash/array').uniq;
+const flatten = require('lodash/array').flatten;
+const compact = require('lodash/array').compact;
+
+const SUPPORTED_FS_FUNCTIONS = new Map()
+  .set('stat',     { returnFirstValue: true })
+  .set('readFile', { returnFirstValue: true })
+  .set('readlink', { returnFirstValue: true })
+  .set('readdir', {
+    returnFirstValue: false,
+
+    mergeResults: (errors, results) => {
+      if (compact(results).length === 0) {
+        return [errors, undefined];
+      } else {
+        const result = unique(compact(flatten(results))).sort();
+        return [undefined, result];
+      }
+    }
+  });
+
+function ensureStartsWithSlash(somepath) {
+  if (somepath[0] !== '/') {
+    somepath = `/${somepath}`;
+  }
+
+  return somepath;
+}
+
+function ensureArray(possiblyArray) {
+  if (!Array.isArray(possiblyArray)) {
+    return [possiblyArray];
+  } else {
+    return possiblyArray;
+  }
+}
+
+
+class MergedFileSystem {
+  constructor(filesystemsByMountPath) {
+    this.mountedPaths = new Map;
+
+    // Itereate over all the mount paths in reverse order (longest first)
+    // and insert into our map (so we can rely on insertion order).
+    Object.keys(filesystemsByMountPath).sort().reverse().forEach(mountPath => {
+      mountPath = ensureStartsWithSlash(mountPath);
+
+      let filesystems = ensureArray(filesystemsByMountPath[mountPath]);
+      this.mountedPaths.set(mountPath, filesystems);
+    });
+
+    for(let [funcName, funcOptions] of SUPPORTED_FS_FUNCTIONS) {
+      this[funcName] = this._callAsyncFunc.bind(this, funcName, funcOptions);
+      this[`${funcName}Sync`] = this._callSyncFunc.bind(this, `${funcName}Sync`, funcOptions);
+    };
+  }
+
+  _gatherStuffToIterateOver(filepath) {
+    const toIterateOver = [];
+
+    // One loop to gather the all mountpaths and filesystems that match
+    // (so we have a total to know when async calls are fully complete)
+    for (let [mountPath, filesystems] of this.mountedPaths) {
+      if (filepath.indexOf(mountPath) === 0) {
+        let subpath;
+
+        if (mountPath === '/') {
+          subpath = filepath;
+        } else {
+          subpath = filepath.slice(mountPath.length);
+        }
+
+        for (let filesystem of filesystems) {
+          let potentialSubpathAlias = subpath;
+          let potentialAliasFS = undefined;
+
+          // Treat "string" filesystems as simple "aliases" to some place on the
+          // native filesystem
+          if (typeof filesystem === 'string') {
+            potentialSubpathAlias = path.join(filesystem, subpath);
+            potentialAliasFS = filesystem;
+            filesystem = nodeFS;
+          }
+
+          toIterateOver.push([
+            mountPath,
+            filesystem,
+            potentialSubpathAlias,
+            potentialAliasFS
+          ])
+        }
+      }
+    }
+
+    return toIterateOver;
+  }
+
+  _iterateOverFilesystemsSync(filepath, iterCallback, doneCallback) {
+    filepath = ensureStartsWithSlash(filepath);
+
+    const toIterateOver = this._gatherStuffToIterateOver(filepath),
+          collectedErrors = [],
+          collectedResults = [];
+
+    for (let [i, [mountPath, filesystem, subpath, aliasIfExist]] of toIterateOver.entries()) {
+      const iterResult = iterCallback(subpath, filesystem);
+
+      // sync iter func calls can return non-undefined value to stop iterating
+      if (iterResult !== undefined) {
+        return iterResult;
+      }
+    }
+  }
+
+  _iterateOverFilesystemsAsync(filepath, iterCallback, doneCallback) {
+    filepath = ensureStartsWithSlash(filepath);
+
+    const toIterateOver = this._gatherStuffToIterateOver(filepath),
+          collectedErrors = [],
+          collectedResults = [],
+          self = this;
+
+    let index = 0;
+
+    function next(iterError, iterResult, stopIterating) {
+      collectedErrors.push(iterError);
+      collectedResults.push(iterResult);
+
+
+      if (stopIterating || index === toIterateOver.length - 1) {
+        doneCallback(
+          collectedErrors.length > 0 ? collectedErrors : undefined,
+          collectedResults
+        );
+      } else {
+        index += 1;
+        iterate();
+      }
+    }
+
+    function iterate() {
+      const [mountPath, filesystem, subpath, aliasIfExist] = toIterateOver[index];
+      iterCallback(subpath, filesystem, next);
+    }
+
+    iterate();
+  }
+
+  _callAsyncFunc(funcName, funcOptions, filepath, ...otherArgs) {
+    const errors = [],
+          results = [],
+          callback = otherArgs.pop()
+
+    let stoppedEarly = false;
+
+    this._iterateOverFilesystemsAsync(filepath, (subpath, filesystem, next) => {
+      if (filesystem[funcName]) {
+
+        filesystem[funcName](subpath, ...otherArgs, (error, result) => {
+          errors.push(error);  // TODO, do something with these?
+          results.push(result);
+
+          if (error) {
+            next(error);
+          } else if (result !== undefined) {
+            if (funcOptions.returnFirstValue) {
+              callback(undefined, result);
+
+              stoppedEarly = true;
+              next(error, result, true);  // final true to stop iterating
+            } else {
+              next(error, result);
+            }
+          }
+        })
+      }
+    }, function doneIteration() {
+      if (funcOptions.mergeResults) {
+        const [mergedError, mergedResult] = funcOptions.mergeResults(errors, results);
+        callback(mergedError, mergedResult);
+
+      } else if (!stoppedEarly) {
+        // If we fell through the whole way and all were errors, make sure we call
+        // the callback with some error (pass them all? just the first/last one?)
+        callback(errors, undefined);
+      }
+
+
+    });
+
+  }
+
+  _callSyncFunc(funcName, funcOptions, filepath) {
+    const errors = [],
+          results = [];
+
+    const finalResult = this._iterateOverFilesystemsSync(filepath, (subpath, filesystem) => {
+      if (filesystem[funcName]) {
+        let result, error;
+
+        try {
+          result = filesystem[funcName](subpath);
+        } catch (e) {
+          error = e;
+        }
+
+        results.push(result);
+        errors.push(error);
+
+        if (funcOptions.returnFirstValue && result !== undefined) {
+          return result;
+        }
+      }
+    });
+
+    if (funcOptions.mergeResults) {
+      const [mergedError, mergedResult] = funcOptions.mergeResults(errors, results);
+
+      if (mergedResult === undefined && mergedError) {
+        throw mergedError;
+      } else {
+        return mergedResult;
+      }
+    } else if (finalResult === undefined) {
+      // it should have returned earlier, must have only been errors, so throw
+      throw errors;
+    } else {
+      return finalResult;
+    }
+  }
+
+}
+
+/* Creates a proxy fs object (mimicing the node fs API) that merges together
+   multiple filesystem instances and/or paths at different parts of the actual filesystem.
+
+      createMergedFileSystem({
+        "/": require('fs'),  // fallthrough to the native filesystem
+
+        // point a "mount point" to a specific FS impl
+        "/mount-point-1": customFSInstance,
+
+        // make an alias, so that `/an-alias/dir/file.txt` ends up hitting `/some/path/file.txt`
+        "/an-alias: "/some/path",
+
+        // have fallbacks for a specific mount point
+        "/another-mount-point": [
+          customFSInstance2,
+          customFSInstance3,
+          "/some/fs/path"
+        ]
+      });
+
+  Only supports the following node FS APIs so far (both sync and async):
+    - stat
+    - readdir
+    - readFile
+    - readlink
+
+  And by default, all of those functions will return the first successful result.
+  However, `readdir` will merge all the successful results together into a single
+  array.
+
+
+  NOTE, intentially completely ignoring windows paths and using unix-style paths (for now?)
+*/
+function createMergedFileSystem(filesystemsByMountPath) {
+  return new MergedFileSystem(filesystemsByMountPath);
+}
+
+module.exports = createMergedFileSystem;
+
---
+++ test/test.js
@@ -0,0 +1,177 @@
+var path = require('path');
+var nodefs = require('fs');
+var should = require('should');
+
+var createMergedFileSystem = require('../index');
+
+var tempDir = '/tmp/temp-MergedFS';
+var tempFilename = 'test-file.txt';
+var tempFilepath = path.join(tempDir, tempFilename);
+
+describe('MergedFS', () => {
+  beforeEach(() => {
+    nodefs.mkdirSync(tempDir);
+    nodefs.writeFileSync(tempFilepath, 'foobar');
+  });
+
+  afterEach(() => {
+    nodefs.unlinkSync(tempFilepath);
+    nodefs.rmdirSync(tempDir);
+  });
+
+  describe('root fallthrough', () => {
+    beforeEach(() => {
+      this.fs = createMergedFileSystem({
+        "/": require('fs')
+      });
+    });
+
+    it('should read files synchronously', () => {
+      var content = this.fs.readFileSync(tempFilepath).toString();
+      content.should.be.a.String();
+      content.length.should.be.greaterThan(0);
+    });
+
+    it('should read files asynchronously', (done) => {
+      this.fs.readFile(tempFilepath, (err, result) => {
+        var content = result.toString();
+        content.should.be.a.String();
+        content.length.should.be.greaterThan(0);
+        done();
+      });
+    });
+
+    it('should stat files synchronously', () => {
+      var stats = this.fs.statSync(tempFilepath)
+      stats.should.be.ok();
+      stats.isFile().should.be.true();
+    });
+
+    it('should stat files asynchronously', (done) => {
+      this.fs.stat(tempFilepath, (err, stats) => {
+        stats.should.be.ok();
+        stats.isFile().should.be.true();
+        done();
+      });
+    });
+
+    it('should read dirs synchronously', () => {
+      var files = this.fs.readdirSync(tempDir);
+      files.should.be.an.Array();
+      files.should.match([
+        tempFilename
+      ]);
+    });
+
+    it('should read dirs asynchronously', (done) => {
+      this.fs.readdir(tempDir, (err, files) => {
+        files.should.be.an.Array();
+        files.should.match([
+          tempFilename
+        ]);
+        done();
+      });
+    });
+
+  });
+
+
+
+  describe('string alias', () => {
+    beforeEach(() => {
+      this.fs = createMergedFileSystem({
+        "/": tempDir
+      });
+    });
+
+    it('should be accessed by path subset', () => {
+      var content = this.fs.readFileSync(tempFilename).toString();
+      content.should.be.a.String();
+    });
+  });
+
+  describe('multiple fs fallthrough', () => {
+    beforeEach(() => {
+      this.fs = createMergedFileSystem({
+        "/fallthrough": ["/NOT REAL", tempDir]
+      });
+    });
+
+    it('should be accessed by path subset', () => {
+      var content = this.fs.readFileSync(path.join("/fallthrough", tempFilename)).toString();
+      content.should.be.a.String();
+    });
+  });
+
+  describe('multiple mount point fallthrough', () => {
+    beforeEach(() => {
+      this.fs = createMergedFileSystem({
+        "/tmp": "/NOT REAL",
+        "/": nodefs
+      });
+    });
+
+    it('should be accessed despite failure in earlier mount point', () => {
+      var content = this.fs.readFileSync(tempFilepath).toString();
+      content.should.be.a.String();
+    });
+  });
+
+  describe('custom filesystems', () => {
+    beforeEach(() => {
+      this.fs = createMergedFileSystem({
+        "/custom": [{
+          readdirSync: function(filepath) {
+            return ["FOOBAR", "YEAH, not a real FS..."]
+          },
+
+          readdir: function(filepath, callback) {
+            callback(undefined, ["FOOBAR", "YEAH, not a real FS..."]);
+          }
+        }, {
+          readdirSync: function(filepath) {
+            return ["Allo", "FOOBAR"]
+          },
+
+          readdir: function(filepath, callback) {
+            callback(undefined, ["Allo", "FOOBAR"]);
+          }
+        }]
+      });
+    });
+
+    it('should readdir synchronously (and merge files)', () => {
+      var files = this.fs.readdirSync(path.join('/custom', tempDir));
+      files.should.be.an.Array();
+      files.should.match(["Allo", "FOOBAR", "YEAH, not a real FS..."]);
+    });
+
+    it('should readdir asynchronously (and merge files)', (done) => {
+      var files = this.fs.readdir(path.join('/custom', tempDir), (error, files) => {
+        files.should.be.an.Array();
+        files.should.match(["Allo", "FOOBAR", "YEAH, not a real FS..."]);
+        done()
+      });
+    });
+  });
+
+  describe('multiple mount point fallthrough', () => {
+    beforeEach(() => {
+      this.fs = createMergedFileSystem({
+        "/many but all broken": ["/fake 1", "/fake 2", "/fake 3"],
+        "/": "/still not here"
+      });
+    });
+
+    it('should throw synchronously', () => {
+      should.throws(() => this.fs.statSync('/many but all broken/NUTHIN'));
+    });
+
+    it('should call async callback with several errors', () => {
+      this.fs.stat('/many but all broken/NUTHIN', (errors) => {
+        errors.length.should.be.equal(4);
+      });
+    });
+  });
+
+});
